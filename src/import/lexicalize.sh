b409b82a1f3f6b997b8eb02fae3e2365a3fbefad
Jaak Pruulmann-Vengerfeldt <jjpp@meso.ee>
2015-01-19 14:10:00 +0000

    priority-union with flag diacritics is a tricky business

--- src/import/lexicalize.sh
+++ src/import/lexicalize.sh
@@ -3,19 +3,35 @@
 # format of input:
 # laste|aed
 # aasta|aeg
+# tolmu|imeja (+ja liide, tehakse asju)
 
 for w in `cut -d ' ' -f1 $1 | cut -d '|' -f 2 | sort -u `
 do 
 	echo
 	echo "! $w:"
-	grep -hrF " $w+" $GTHOME/langs/est/src/morphology/ | \
+	EXTRA=""
+	case "$w" in
+		*ja)
+			EXTRA="$EXTRA|:\\+ja "
+		;;
+	esac
+	[ -z "$EXTRA" ] || echo "! ($EXTRA)"
+	grep -hrE " $w\\+[A-Z]$EXTRA" $GTHOME/langs/est/src/morphology/ | \
 		sort -u | \
 		grep -v '^[[:space:]]*!' | \
 		while read row; do
 			echo "!  $row"
 			for pref in `egrep '\|'"$w"'\b' $1 | cut -d'|' -f1`; do
 				echo "!   $pref$w:"
-				echo $row | sed -e "s/^/$pref/" -e "s/:/:$pref/"
+				case $w in
+					*ja)
+						w_=`echo $w | sed -e 's/ja$//'`
+						echo $row | sed -e "s/^/$pref$w_/" -e "s/:+/:$pref$w_/"
+					;;
+					*)
+						echo $row | sed -e "s/^/$pref/" -e "s/:/:$pref/"
+					;;
+				esac
 			done
 		done
 done
